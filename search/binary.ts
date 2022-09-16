d66bf3e3fc4e798d7bd48f57c15a492dfddae44c
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2022-09-16 10:00:00 -0300

    Search block

--- search/binary.ts
+++ search/binary.ts
@@ -1,3 +1,12 @@
+/**
+ * "If the needle is in the haystack, return true, otherwise return false."
+ *
+ * The function starts by setting the high and low values to the length of the haystack and 0
+ * respectively
+ * @param {number[]} haystack - the array we're searching through
+ * @param {number} needle - The value we're looking for
+ * @returns A boolean value
+ */
 export default function binarySearch(haystack: number[], needle: number): boolean {
   let high = haystack.length
   let low = 0
